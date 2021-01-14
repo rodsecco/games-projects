c02ac3f93136f975e63853d7fdc601a02477f7d6
Rodrigo Secco <rodsecco@gmail.com>
2021-01-14 15:59:17 -0300

    New ending message

--- forca.py
+++ forca.py
@@ -22,10 +22,11 @@ def jogar():
         else:
             errors += 1
             set_wrong_guess(errors, max_tries)
+            desenha_forca(errors)
 
         print(guessed_letters)
 
-        gameover = errors == max_tries
+        gameover = errors > max_tries
         guessed = "_" not in guessed_letters
 
     call_end_game(guessed, secret_word)
@@ -69,10 +70,87 @@ def set_wrong_guess(errors, max_tries):
 
 def call_end_game(guessed, secret_word):
     if(guessed):
-        print("Correto, você ganhou!! Palavra secreta: {}".format(secret_word))
+        print("Correto, você ganhou!! Palavra secreta: '{}'".format(secret_word))
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")
     else:
-        print("Que pena, você perdeu! Palavra secreta: {}".format(secret_word))    
+        print("Que pena, você foi enforcado!")
+        print("A palavra era '{}'".format(secret_word))
+        print("    _______________         ")
+        print("   /               \       ")
+        print("  /                 \      ")
+        print("//                   \/\  ")
+        print("\|   XXXX     XXXX   | /   ")
+        print(" |   XXXX     XXXX   |/     ")
+        print(" |   XXX       XXX   |      ")
+        print(" |                   |      ")
+        print(" \__      XXX      __/     ")
+        print("   |\     XXX     /|       ")
+        print("   | |           | |        ")
+        print("   | I I I I I I I |        ")
+        print("   |  I I I I I I  |        ")
+        print("   \_             _/       ")
+        print("     \_         _/         ")
+        print("       \_______/           ")   
     print("Fim de jogo!!")
+
+def desenha_forca(errors):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(errors == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(errors == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(errors == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(errors == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(errors == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(errors == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (errors == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 if(__name__ == "__main__"):
     jogar()
